b9aa07e17db49536255ad4ecb6fa9fc80cd13cd2
madhumita Bharde <madhumita25@github.com>
2015-10-21 12:28:53 +0530

    predictions

--- predict_collection.py
+++ predict_collection.py
@@ -11,7 +11,7 @@ import re
 weight = [1,2,3,4]
 
 def estimate_fill1(binId, threshold):
-    binFile = "/Users/bharde/BinWatch/BinWatch/" + binId;
+    binFile = "/Users/bharde/BinWatch/BinWatch/" + binId + ".csv";
     sum_weight = 0;
     fill = [0] * 10
     timestamp = [0] * 10
@@ -20,7 +20,6 @@ def estimate_fill1(binId, threshold):
     with open(binFile, newline='') as f:
         reader = csv.reader(f, delimiter=',')
         for row in reader:
-            #print(row[4], row[5])
             fill[i] = int(row[4])
             timestamp[i] = float(row[5])
             if i != 0 and (fill[i] > fill[i-1]):
@@ -33,16 +32,16 @@ def estimate_fill1(binId, threshold):
     time = (threshold - fill[i-1])/rate;
 
     predicted_timestamp = timestamp[i-1] + time;
+
     #current_timestamp = timestamp = datetime.datetime.utcnow().timestamp();
-
+    #if predicted_timestamp <  current_timestamp:
+        #predicted_timestamp = current_timestamp;
     #print (predicted_timestamp)
+
     return predicted_timestamp
-    if predicted_timestamp <  current_timestamp:
-        predicted_timestamp = current_timestamp;
 
-    #print (predicted_timestamp)
-
-listFile = "/Users/bharde/BinWatch/BinWatch/bin_list.csv"
+#listFile = "/Users/bharde/BinWatch/BinWatch/bin_list.csv"
+listFile = sys.argv[1];
 min_time = 1443493164;
 #min_time = now;
 i = 0;
@@ -51,10 +50,8 @@ threshold = 90;
 with open(listFile, 'r') as dbFile:
     lines = dbFile.readlines()
     for row in lines:
-        row = re.split('\s+', row)
-        #print(row[8]);
-        time = estimate_fill1(row[8], threshold)
-        #print ( time, min_time)
+        print(row);
+        time = estimate_fill1(row.rstrip(), threshold)
         if time < min_time:
             min_time = time;
 print ("Next Collect at", min_time);
